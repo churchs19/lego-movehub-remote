1fe0297d9d4d82236f6ef2fa1107b3d7b704ab01
Shane Church <shane@s-church.net>
2019-01-08 17:55:35 -0600

    Debugging remote

--- experiments/src/vernie-remote.ts
+++ experiments/src/vernie-remote.ts
@@ -2,23 +2,18 @@ import PoweredUP = require('node-poweredup');
 
 class VernieRemote {
     private poweredUP = new PoweredUP.PoweredUP();
+    private poweredUP2 = new PoweredUP.PoweredUP();
+    private boostHub: PoweredUP.BoostMoveHub;
+    private remote: PoweredUP.PUPRemote;
 
     public init() {
         this.poweredUP.on('discover', async (hub: PoweredUP.Hub) => {
-            let boostHub: PoweredUP.BoostMoveHub;
-            let remote: PoweredUP.PUPRemote;
-
-            if (hub instanceof PoweredUP.BoostMoveHub) {
-                await hub.connect();
-                boostHub = hub;
+            if (hub instanceof PoweredUP.BoostMoveHub && !this.boostHub) {
+                this.boostHub = hub;
+                await this.boostHub.connect();
+                console.log(`Connected to ${hub.name} of type ${PoweredUP.Consts.HubType[hub.type]}!`);
             }
-
-            if (hub instanceof PoweredUP.PUPRemote) {
-                await hub.connect();
-                remote = hub;
-            }
-
-            console.log(`Connected to ${hub.name} of type ${PoweredUP.Consts.HubType[hub.type]}!`);
+            this.poweredUP.stop();
         });
         this.poweredUP.scan();
 
